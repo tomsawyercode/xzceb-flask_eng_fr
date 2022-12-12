821e63993e9f1dbc1256a24cfb8ac6f9c94de3a8
Your Name <you@example.com>
2022-12-12 10:24:45 -0300

    update /test.py

--- final_project/machinetranslation/tests/test.py
+++ final_project/machinetranslation/tests/test.py
@@ -7,13 +7,14 @@ class TestE2F(unittest.TestCase):
         self.assertEqual(english_to_french(None), None) 
         self.assertEqual(english_to_french('Hello'),'Bonjour')
         self.assertEqual(english_to_french('Night'),'Nuit')
+        self.assertNotEqual(english_to_french('Night'),'Night')
         
 class TestF2E(unittest.TestCase): 
     def test1(self): 
         self.assertEqual(french_to_english(None), None) 
         self.assertEqual(french_to_english('Bonjour'),'Hello')
         self.assertEqual(french_to_english('Nuit'),'Night')
-        
+        self.assertNotEqual(french_to_english('Nuit'),'Nuit')        
   
 unittest.main()
 
